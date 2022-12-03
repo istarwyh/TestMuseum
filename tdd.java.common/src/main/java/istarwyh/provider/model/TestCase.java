0570db525469da7ff96b98b3ddc5095130cc01db
istarwyh <yihui.wang@pku.edu.cn>
2022-12-03 21:01:06 +0800

    Add:should_parse_People_type_test_case_with_direct_address

--- tdd.java.common/src/main/java/istarwyh/provider/model/TestCase.java
+++ tdd.java.common/src/main/java/istarwyh/provider/model/TestCase.java
@@ -1,14 +1,65 @@
 package istarwyh.provider.model;
 
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONArray;
+import com.alibaba.fastjson2.JSONObject;
+
 public class TestCase<IN, OUT> {
 
     /**
      * input args of function
      */
-    public IN input;
+    private IN input;
 
     /**
      * output result of function
      */
-    public OUT output;
+    private OUT output;
+
+
+    /**
+     *
+     * @param inType not primitive type like String,Integer,List
+     * @return {@link IN}
+     */
+    public IN getInput(Class<IN> inType) {
+        if(isJSON(input)){
+            return JSON.parseObject(JSON.toJSONString(input),inType) ;
+        }else {
+            return input ;
+        }
+    }
+
+    /**
+     *
+     * @param outType not primitive type like String,Integer,List
+     * @return {@link OUT}
+     */
+    public OUT getOutput(Class<OUT> outType) {
+        if(isJSON(output)){
+            return JSON.parseObject(JSON.toJSONString(output),outType) ;
+        }else {
+            return output ;
+        }
+    }
+
+    public <T> boolean isJSON(T o){
+        return o instanceof JSONObject || o instanceof JSONArray;
+    }
+
+    public IN getInput() {
+        return input;
+    }
+
+    public OUT getOutput() {
+        return output;
+    }
+
+    public void setInput(IN input) {
+        this.input = input;
+    }
+
+    public void setOutput(OUT output) {
+        this.output = output;
+    }
 }
--- tdd.java.common/src/test/java/istarwyh/provider/JsonFileArgumentsProviderTest.java
+++ tdd.java.common/src/test/java/istarwyh/provider/JsonFileArgumentsProviderTest.java
@@ -26,29 +26,36 @@ class JsonFileArgumentsProviderTest {
     @ParameterizedTest
     @JsonFileSource(type = TestCase.class,resources = {"/list_testCase.json"})
     void should_parse_List_type_test_case(TestCase<List<Integer>,List<Integer>> testCase){
-        assertEquals("[1,2,3]",testCase.input.toString());
-        assertEquals("[3,4,5]",testCase.output.toString());
+        assertEquals("[1,2,3]",testCase.getInput().toString());
+        assertEquals("[3,4,5]",testCase.getOutput().toString());
     }
 
     @ParameterizedTest
     @JsonFileSource(type = TestCase.class,resources = {"/list_testCase.json", "/list_testCase.json"})
     void should_parse_multi_List_with_Integer_type_test_case(TestCase<List<Integer>,List<Integer>> testCase){
-        assertEquals(1,testCase.input.get(0));
-        assertEquals(5,testCase.output.get(2));
+        assertEquals(1,testCase.getInput().get(0));
+        assertEquals(5,testCase.getOutput().get(2));
     }
 
     @ParameterizedTest
     @JsonFileSource(type = TestCase.class,resources = {"/map_testCase.json"})
     void should_parse_Map_type_test_case(TestCase<Map<String,Integer>,Map<String,Integer>> testCase){
-        assertEquals(1,testCase.input.get("key1"));
-        assertEquals(2,testCase.output.get("key2"));
+        assertEquals(1,testCase.getInput().get("key1"));
+        assertEquals(2,testCase.getOutput().get("key2"));
     }
 
     @ParameterizedTest
     @JsonFileSource(type = TestCase.class,resources = {"map_testCase.json"})
     void should_parse_Map_type_test_case_with_direct_address(TestCase<Map<String,Integer>,Map<String,Integer>> testCase){
-        assertEquals(1,testCase.input.get("key1"));
-        assertEquals(2,testCase.output.get("key2"));
+        assertEquals(1,testCase.getInput().get("key1"));
+        assertEquals(2,testCase.getOutput().get("key2"));
+    }
+
+    @ParameterizedTest
+    @JsonFileSource(type = TestCase.class,resources = {"istarwyh.provider/people_list_testCase.json"})
+    void should_parse_People_type_test_case_with_direct_address(TestCase<People,People> testCase){
+        assertEquals("lele",testCase.getInput(People.class).name);
+        assertEquals("02",testCase.getOutput(People.class).id);
     }
 
     @Nested
@@ -61,7 +68,7 @@ class JsonFileArgumentsProviderTest {
             assertEquals("lele",peopleList.get(0).name);
             assertEquals("02",peopleList.get(1).id);
         }
-
-        public record People(String id,String name){}
     }
+    public record People(String id,String name){}
+
 }
