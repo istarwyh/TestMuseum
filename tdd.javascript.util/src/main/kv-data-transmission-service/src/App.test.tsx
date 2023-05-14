d9f9e08a9090ea0ed64f5cdad029568d7c46089f
istarwyh <yihui.wang@pku.edu.cn>
2023-05-14 17:19:46 +0800

    Add: TableStoreDataTransmissionServiceConfigGenerate

--- tdd.javascript.util/src/main/kv-data-transmission-service/src/App.test.tsx
+++ tdd.javascript.util/src/main/kv-data-transmission-service/src/App.test.tsx
@@ -1,9 +1,71 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {render, screen} from '@testing-library/react';
+import TableStoreDataTransmissionServiceConfigGenerate, {extractTableInfo} from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    render(<TableStoreDataTransmissionServiceConfigGenerate/>);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+});
+
+const validDDL = `
+    CREATE TABLE supplier_bill
+    (
+        id              INT PRIMARY KEY,
+        supplier_name   STRING,
+        bill_no         STRING,
+        amount          DECIMAL,
+        settlement_date STRING,
+        bill_type       STRING,
+        supplier_id     INT,
+        entry_time      TIMESTAMP
+    )`;
+
+const invalidDDL = 'CREATE INDEX ON mytable (name)';
+
+describe('extractTableAndColumns', () => {
+    test('should extract table name and columns from valid DDL statement', () => {
+        const result = extractTableInfo(validDDL);
+        expect(result).toEqual({
+            tableName: 'supplier_bill',
+            columns: [
+                {
+                    "name": "id",
+                    "type": "INT"
+                },
+                {
+                    "name": "supplier_name",
+                    "type": "STRING"
+                },
+                {
+                    "name": "bill_no",
+                    "type": "STRING"
+                },
+                {
+                    "name": "amount",
+                    "type": "DECIMAL"
+                },
+                {
+                    "name": "settlement_date",
+                    "type": "STRING"
+                },
+                {
+                    "name": "bill_type",
+                    "type": "STRING"
+                },
+                {
+                    "name": "supplier_id",
+                    "type": "INT"
+                },
+                {
+                    "name": "entry_time",
+                    "type": "TIMESTAMP"
+                }
+            ]
+        });
+    });
+
+    test('should throw error for invalid DDL statement', () => {
+        expect(() => extractTableInfo(invalidDDL)).toThrowError("DDL 解析错误");
+    });
 });
--- tdd.javascript.util/src/main/kv-data-transmission-service/src/App.tsx
+++ tdd.javascript.util/src/main/kv-data-transmission-service/src/App.tsx
@@ -1,26 +1,189 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Button, Col, Input, Row, Space, Typography} from 'antd';
+import {get} from 'http';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const {TextArea} = Input;
+const {Title} = Typography;
+
+
+interface State {
+    operator: string;
+    partition: string;
+    schema: string;
+    odpsSpace: string;
+    tableStoreSpace: string;
+    tableStoreName: string;
+    primaryKeyCount: number;
+    jsonConfig: string;
 }
 
-export default App;
+interface TableInfo {
+    tableName: string;
+    columns: {
+        name: string,
+        type: string
+    }[];
+}
+
+export function extractTableInfo(ddl: string) {
+    const tableRegex = /create\s+table\s+(\w+)\s+/i;
+    const columnRegex = /\(([\s\S]*?)\)/;
+
+    const tableMatch = ddl.match(tableRegex);
+    const columnMatch = ddl.match(columnRegex);
+
+    if (!tableMatch || !columnMatch) {
+        throw new Error('DDL 解析错误');
+    }
+
+    const tableName = tableMatch[1];
+    const columnText = columnMatch[1];
+
+    const columns = columnText.split(',').map((c) => {
+        const [name, type] = c.trim().split(/\s+/);
+        return {name, type};
+    });
+
+    return {
+        tableName,
+        columns,
+    };
+}
+
+function generateJsonConfigObj(tableInfo: TableInfo, state:State) {
+    const {tableName, columns} = tableInfo;
+    console.log(JSON.stringify(tableInfo));
+    const {operator, partition, odpsSpace, tableStoreSpace, tableStoreName,primaryKeyCount} = state;
+    return {
+        baseId: operator,
+        type: "job",
+        version: "2.0",
+        steps: [
+            {
+                stepType: "odps",
+                parameter: {
+                    partition: partition,
+                    isCompress: false,
+                    indexes: [],
+                    datasource: "_ODPS",
+                    envType: 1,
+                    isSupportThreeModel: false,
+                    column: columns.map(it => it),
+                    guid: `${odpsSpace}.${tableName}`,
+                    table: tableName,
+                }
+            },
+            {
+                stepType: "ots",
+                parameter: {
+                    datasource: tableStoreSpace,
+                    envType: 1,
+                    primaryKey: columns.slice(0,3).map(it => it),
+                    column: columns.slice(primaryKeyCount,columns.length).map(it => it),
+                    table: tableStoreName,
+                    writeMode: "PutRow"
+                },
+                name:"Writer",
+                category:"writer"
+            },
+            {
+                copies:1,
+                parameter:{nodes:[],edges:[],groups:[],version:"2.0"},
+                name:"Processor",
+                category:"processor"
+            }
+        ],
+        setting:{
+            jvmOption:"",errorLimit:{record:"0"},locale:"zh",speed:{throttle:false,concurrent:3}
+        },
+        order:{
+          hops:[{
+              from:"Reader",to:"Writer"
+          }]
+        }
+    }
+}
+
+const TableStoreDataTransmissionServiceConfigGenerate = (): JSX.Element => {
+    const [state, setState] = React.useState<State>({
+        operator: '',
+        partition: "ds = $bizdate",
+        schema: '',
+        odpsSpace: 'odps.c2m_supply',
+        tableStoreSpace: '',
+        tableStoreName: '',
+        primaryKeyCount: 3,
+        jsonConfig: ''
+    });
+    const {operator, odpsSpace, partition, schema, jsonConfig, tableStoreName,tableStoreSpace,primaryKeyCount} = state;
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = event.target;
+        setState({...state, [name]: value});
+    };
+
+
+    const generateJsonConfig = () => {
+        const tableInfo = extractTableInfo(schema.trim());
+        const jsonConfigObj = generateJsonConfigObj(tableInfo,state);
+        const jsonConfig = JSON.stringify(jsonConfigObj, null, 2);
+        setState(
+            (prevState) => {
+                return ({...prevState, jsonConfig});
+            }
+        );
+    };
+
+    return (
+        <div className="container">
+            <Title level={3}>TableStore数据同步配置器</Title>
+            <Space direction="vertical" className={"space"}>
+                <Row>
+                    <Col span={6}>
+                        <Input name="operator" value={operator} onChange={handleChange} placeholder="操作者工号"/>
+                    </Col>
+                    <Col span={12}>
+                        <Input name="odpsSpace" value={odpsSpace} onChange={handleChange}
+                               placeholder={`项目空间名,默认 ${odpsSpace}`}/>
+                    </Col>
+                    <Col span={6}>
+                        <Input name="partition" value={partition} onChange={handleChange}
+                               placeholder={`分区信息,默认 ${partition}`}/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={6}>
+                        <Input name="tableStoreSpace" value={tableStoreSpace} onChange={handleChange}
+                               placeholder={`TableSore 实例名`}/>
+                    </Col>
+                    <Col span={12}>
+                        <Input name="tableStoreName" value={tableStoreName} onChange={handleChange}
+                               placeholder={`TableSore 表名`}/>
+                    </Col>
+                    <Col span={6}>
+                        <Input name="primaryKeyCount" value={primaryKeyCount} onChange={handleChange}
+                               placeholder={`主键个数,最大4`}/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={12}>
+                        <Input name="schema" value={schema} onChange={handleChange} placeholder="DDL语句"/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={12}>
+                        <Button type={"primary"} onClick={generateJsonConfig}
+                                className={"generation-button"}>生成同步配置</Button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={12}>
+                        <TextArea value={jsonConfig} className={"text-area"}/>
+                    </Col>
+                </Row>
+            </Space>
+        </div>
+    );
+};
+
+export default TableStoreDataTransmissionServiceConfigGenerate;
