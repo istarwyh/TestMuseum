fbc42fc331a159f63d8d2aa02dfa3008898e5ed6
istarwyh <yihui.wang@pku.edu.cn>
2024-05-11 08:53:27 +0800

    modify element

--- tdd.java.common/src/main/java/istarwyh/page_module_loader/component/AbstractElement.java
+++ tdd.java.common/src/main/java/istarwyh/page_module_loader/component/AbstractElement.java
@@ -1,15 +1,15 @@
 package istarwyh.page_module_loader.component;
 
+import static java.util.Optional.ofNullable;
+
 import com.google.common.base.CaseFormat;
-import istarwyh.page_module_loader.PageModule;
 import istarwyh.page_module_loader.ElementDTO;
+import istarwyh.page_module_loader.PageModule;
+import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.List;
-
 
 /**
  * @author xiaohui
@@ -19,33 +19,31 @@ import java.util.List;
 @NoArgsConstructor
 public abstract class AbstractElement<DATA> extends ElementDTO implements PageModule<DATA> {
 
-    private DATA data;
+  private DATA data;
 
-    @Override
-    public String getModuleTypeCode(){
-        return CaseFormat.LOWER_CAMEL.to(CaseFormat.UPPER_UNDERSCORE, this.getClass().getSimpleName());
-    }
+  @Override
+  public String getModuleTypeCode() {
+    return CaseFormat.LOWER_CAMEL.to(CaseFormat.UPPER_UNDERSCORE, this.getClass().getSimpleName());
+  }
 
-    @Override
-    public @NotNull DATA getData() {
-        return data;
-    }
+  @Override
+  public @NotNull DATA getData() {
+    return data;
+  }
 
-    @Override
-    public void setData(Object data) {
-        this.data = (DATA) data;
-    }
+  @Override
+  public void setData(Object data) {
+    this.data = (DATA) data;
+  }
 
-    public <ELEMENT extends AbstractElement<?>> void fillWith(ELEMENT materiaElement){
-        if (materiaElement == null) {
-            return;
-        }
-        this.setAmount(materiaElement.getAmount());
-        this.setNumber(materiaElement.getNumber());
-        this.setTime(materiaElement.getTime());
-        List<ElementDTO> details = materiaElement.getDetails();
-        if (details != null && !details.isEmpty()) {
-            this.setData(details);
-        }
+  public <ELEMENT extends AbstractElement<?>> void fillWith(ELEMENT materiaElement) {
+    if (materiaElement == null) {
+      return;
     }
+    this.setAmount(ofNullable(this.getAmount()).orElse(materiaElement.getAmount()));
+    this.setNumber(ofNullable(this.getNumber()).orElse(materiaElement.getNumber()));
+    this.setTime(ofNullable(this.getTime()).orElse(materiaElement.getTime()));
+    List<ElementDTO> details = materiaElement.getDetails();
+    this.setData(ofNullable(this.getData()).orElse((DATA) details));
+  }
 }
